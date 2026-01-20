a01b9c0302d75b46d223f0e0056cbbddca7d4454
wangh <wanghao0628@hotmail.com>
2026-01-20 21:33:12 +1300

    Add STATIC_URL and STATIC_ROOT for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -130,5 +130,6 @@ CORS_ALLOWED_ORIGINS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-#STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
